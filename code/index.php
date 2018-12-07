d97b1c31cb98dea1f8b1a707bb738b1e704d10f9
Nelson Parreira <nelsonblparreira@gmail.com>
2018-12-07 11:42:27 +0100

    Added login form

--- code/index.php
+++ code/index.php
@@ -15,8 +15,31 @@
         
 
 
+            <div class="uk-section uk-container">
+                <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+                    <form class="uk-form-stacked js-login">
 
+                        <div class="uk-margin">
+                            <label class="uk-form-label" for="form-stacked-text">Email</label>
+                            <div class="uk-form-controls">
+                                <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
+                            </div>
+                        </div>
 
+                        <div class="uk-margin">
+                            <label class="uk-form-label" for="form-stacked-text">Password</label>
+                            <div class="uk-form-controls">
+                                <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
+                            </div>
+                        </div>
+
+                        <div class="uk-margin">
+                            <button class="uk-button uk-button-default" type="submit">Login</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
         
         <!-- jQuery is required -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
