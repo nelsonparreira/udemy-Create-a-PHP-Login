879a3081f017b9395f40a6ff96c489fca3fcd9c2
Nelson Parreira <nelsonblparreira@gmail.com>
2018-12-07 11:01:25 +0100

    add Exercide 7 and prepared the inicial file index.php

--- code/index.php
+++ code/index.php
@@ -1,1 +1,28 @@
-<?php echo "Hello World"; ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="follow">
+
+        <title>Page Title</title>
+
+        <base href="/" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+    </head>
+    <body>
+        
+
+
+
+
+        
+        <!-- jQuery is required -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- UIkit JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
+    </body>
+</html>
